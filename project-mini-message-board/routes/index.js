ea2643df333dae777b6e698dded1ad3427d817fd
Lukas <lukasdrinkwater97@gmail.com>
2023-10-26 21:36:02 +0100

    started working through mini message board project and adding better styling

---
+++ project-mini-message-board/routes/index.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages });
+});
+
+router.get("/new", (req, res) => {
+  res.render("form", {});
+});
+
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  // res.send(`Author: ${req.body.author}, Message: ${req.body.message}`);
+  // sends the user back to the message page
+  res.redirect("/");
+});
+
+module.exports = router;
